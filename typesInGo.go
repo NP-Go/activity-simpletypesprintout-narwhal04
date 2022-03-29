e34245ccceea45cea9de1699e5f2a5e6d6c0d928
narwhal04 <q.quek7@gmail.com>
2022-03-29 22:29:08 +0800

    vars added

--- typesInGo.go
+++ typesInGo.go
@@ -1,16 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 //Insert variables declarations here based on activity
 var (
-	text      string = "The following is the account information"
-	firstname string = "Luke"
-	lastname  string = "SkyWalker"
+	text             string  = "The following is the account information"
+	firstname        string  = "Luke"
+	lastname         string  = "SkyWalker"
+	age              int     = 20
+	weight           float32 = 73.0
+	height           float32 = 1.72
+	remainingCredits float32 = 123.55
+	accountName      string  = "admin"
+	accountPassword  string  = "password"
+	user             bool    = true
 )
 
 func tellMeTypes() {
-	fmt.Printf("%T %T %T", text, firstname, lastname)
+	fmt.Printf("%T %T %T %T %T %T %T %T %T %T", text, firstname, lastname, age, weight, height, remainingCredits, accountName, accountPassword, user)
+
 }
 
 func main() {
